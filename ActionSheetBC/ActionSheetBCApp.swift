5613602d196101c232ca1675840441cc5f253036
githubuiux <uiuxista@gmail.com>
2023-07-31 13:26:19 +0300

    Initial Commit

---
+++ ActionSheetBC/ActionSheetBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSheetBCApp.swift
+//  ActionSheetBC
+//
+//  Created by Levent on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ActionSheetBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
